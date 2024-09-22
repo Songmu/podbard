68c8f15fae07fbb8545e849639d123ebf3213656
Songmu <y.songmu@gmail.com>
2024-09-22 17:38:59 +0900

    introduce urfave/cli/v2 for flags

--- cmd_episode.go
+++ cmd_episode.go
@@ -1,9 +1,7 @@
 package podbard
 
 import (
-	"context"
 	"errors"
-	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -14,87 +12,110 @@ import (
 	"github.com/Songmu/go-httpdate"
 	"github.com/Songmu/podbard/internal/cast"
 	"github.com/mattn/go-isatty"
+	"github.com/urfave/cli/v2"
 )
 
-type cmdEpisode struct {
-}
+var commandEpisode = &cli.Command{
+	Name:  "episode",
+	Usage: "manage episodes",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:  "slug",
+			Usage: "slug of the episode",
+		},
+		&cli.StringFlag{
+			Name:  "date",
+			Usage: "date of the episode",
+		},
+		&cli.StringFlag{
+			Name:  "title",
+			Usage: "title of the episode",
+		},
+		&cli.StringFlag{
+			Name:  "description",
+			Usage: "description of the episode",
+		},
+		&cli.BoolFlag{
+			Name:  "no-edit",
+			Usage: "do not open the editor",
+		},
+		&cli.BoolFlag{
+			Name:  "ignore-missing",
+			Usage: "ignore missing audio file",
+		},
+		&cli.BoolFlag{
+			Name:  "save-meta",
+			Usage: "save meta file of audio",
+		},
+	},
+	Action: func(c *cli.Context) error {
+		rootDir := c.String("C")
 
-func (cd *cmdEpisode) Command(ctx context.Context, args []string, outw, errw io.Writer) error {
-	flagCfg := getFlagConfig(ctx)
-	rootDir := flagCfg.RootDir
+		args := c.Args().Slice()
+		if len(args) < 1 {
+			return errors.New("no audio files specified")
+		}
+		audioFiles := args
 
-	fs := flag.NewFlagSet("podbard episode", flag.ContinueOnError)
-	fs.SetOutput(errw)
-	var (
-		slug        = fs.String("slug", "", "slug of the episode")
-		date        = fs.String("date", "", "date of the episode")
-		title       = fs.String("title", "", "title of the episode")
-		descripsion = fs.String("description", "", "description of the episode")
+		slug := c.String("slug")
+		date := c.String("date")
+		title := c.String("title")
+		description := c.String("description")
+		noEdit := c.Bool("no-edit")
+		ignoreMissing := c.Bool("ignore-missing")
+		saveMeta := c.Bool("save-meta")
 
-		noEdit        = fs.Bool("no-edit", false, "do not open the editor")
-		ignoreMissing = fs.Bool("ignore-missing", false, "ignore missing audio file")
-		saveMeta      = fs.Bool("save-meta", false, "save meta file of audio")
-	)
-	if err := fs.Parse(args); err != nil {
-		return err
-	}
-	if fs.NArg() < 1 {
-		return errors.New("no audio files specified")
-	}
-	audioFiles := fs.Args()
-
-	cfg, err := cast.LoadConfig(rootDir)
-	if err != nil {
-		return err
-	}
-	loc := cfg.Location()
-
-	var pubDate time.Time
-	if *date != "" {
-		var err error
-		pubDate, err = httpdate.Str2Time(*date, loc)
+		cfg, err := cast.LoadConfig(rootDir)
 		if err != nil {
 			return err
 		}
-	}
-	var body string
-	if !isTTY(os.Stdin) {
-		b, err := io.ReadAll(os.Stdin)
-		if err != nil {
-			return err
-		}
-		body = string(b)
-	}
-
-	editor := os.Getenv("EDITOR")
-	if editor == "" || *noEdit || !isTTY(os.Stdin) || !isTTY(os.Stdout) || !isTTY(os.Stderr) {
-		editor = ""
-	}
-	for _, audioFile := range audioFiles {
-		fpath, isNew, err := cast.LoadEpisode(
-			rootDir, audioFile, body, *ignoreMissing, *saveMeta, pubDate, *slug, *title, *descripsion, loc)
-		if err != nil {
-			return err
-		}
-		if isNew {
-			log.Printf("📝 The episode file %q corresponding to the %q was created.\n", fpath, audioFile)
-		} else {
-			log.Printf("🔍 The episode file %q corresponding to the %q was found.\n", fpath, audioFile)
-		}
-		fmt.Fprintln(outw, fpath)
-
-		if editor != "" {
-			com := exec.Command(editor, fpath)
-			com.Stdin = os.Stdin
-			com.Stdout = os.Stdout
-			com.Stderr = os.Stderr
-
-			if err := com.Run(); err != nil {
+		loc := cfg.Location()
+		var pubDate time.Time
+		if date != "" {
+			var err error
+			pubDate, err = httpdate.Str2Time(date, loc)
+			if err != nil {
 				return err
 			}
 		}
-	}
-	return nil
+		var body string
+		if !isTTY(os.Stdin) {
+			b, err := io.ReadAll(os.Stdin)
+			if err != nil {
+				return err
+			}
+			body = string(b)
+		}
+		editor := os.Getenv("EDITOR")
+		if editor == "" || noEdit || !isTTY(os.Stdin) || !isTTY(os.Stdout) || !isTTY(os.Stderr) {
+			editor = ""
+		}
+		for _, audioFile := range audioFiles {
+			fpath, isNew, err := cast.LoadEpisode(
+				rootDir, audioFile, body, ignoreMissing, saveMeta, pubDate, slug, title, description, loc)
+			if err != nil {
+				return err
+			}
+			if isNew {
+				log.Printf("📝 The episode file %q corresponding to the %q was created.\n", fpath, audioFile)
+			} else {
+				log.Printf("🔍 The episode file %q corresponding to the %q was found.\n", fpath, audioFile)
+			}
+			fmt.Fprintln(c.App.Writer, fpath)
+
+			if editor != "" {
+				com := exec.Command(editor, fpath)
+				com.Stdin = os.Stdin
+				com.Stdout = os.Stdout
+				com.Stderr = os.Stderr
+
+				if err := com.Run(); err != nil {
+					return err
+				}
+			}
+		}
+		return nil
+	},
 }
 
 func isTTY(f *os.File) bool {
--- podbard.go
+++ podbard.go
@@ -2,11 +2,11 @@ package podbard
 
 import (
 	"context"
-	"errors"
-	"flag"
 	"fmt"
 	"io"
 	"log"
+
+	"github.com/urfave/cli/v2"
 )
 
 const cmdName = "podbard"
@@ -14,45 +14,22 @@ const cmdName = "podbard"
 // Run the podbard
 func Run(ctx context.Context, argv []string, outw, errw io.Writer) error {
 	log.SetOutput(errw)
-	fs := flag.NewFlagSet(
-		fmt.Sprintf("%s (v%s rev:%s)", cmdName, version, revision), flag.ContinueOnError)
-	fs.SetOutput(errw)
 
-	ver := fs.Bool("version", false, "display version")
-	rootDir := fs.String("C", ".", "change to directory")
-	if err := fs.Parse(argv); err != nil {
-		return err
+	app := cli.NewApp()
+	app.Writer = outw
+	app.ErrWriter = errw
+	app.Version = fmt.Sprintf("%s v%s (rev:%s)", cmdName, version, revision)
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:  "C",
+			Value: ".",
+			Usage: "change to directory",
+		},
 	}
-	if *ver {
-		return printVersion(outw)
+	app.Commands = []*cli.Command{
+		commandInit,
+		commandEpisode,
+		commandBuild,
 	}
-
-	argv = fs.Args()
-	if len(argv) < 1 {
-		return errors.New("no subcommand specified")
-	}
-	com, ok := commands[argv[0]]
-	if !ok {
-		return fmt.Errorf("unknown subcommand: %s", argv[0])
-	}
-	return com.Command(
-		withFlagConfig(ctx, &flagConfig{
-			RootDir: *rootDir,
-		}),
-		argv[1:], outw, errw)
-}
-
-func printVersion(out io.Writer) error {
-	_, err := fmt.Fprintf(out, "%s v%s (rev:%s)\n", cmdName, version, revision)
-	return err
-}
-
-var commands = map[string]commander{
-	"init":    &cmdInit{},
-	"episode": &cmdEpisode{},
-	"build":   &cmdBuild{},
-}
-
-type commander interface {
-	Command(ctx context.Context, args []string, outw, errw io.Writer) error
+	return app.RunContext(ctx, append([]string{cmdName}, argv...))
 }
